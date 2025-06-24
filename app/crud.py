a7e6dab256c1d8896997131d3213dc0a56bd7d29
Atul Rajput <eatulrajput.com>
2025-06-24 08:17:09 +0530

    Add Testing

--- app/crud.py
+++ app/crud.py
@@ -8,7 +8,7 @@ def get_book(db: Session, book_id: int):
     return db.query(models.Book).filter(models.Book.id == book_id).first()
 
 def create_book(db: Session, book: schemas.BookCreate):
-    db_book = models.Book(**book.dict())
+    db_book = models.Book(**book.model_dump())
     db.add(db_book)
     db.commit()
     db.refresh(db_book)
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class BookBase(BaseModel):
     title: str
@@ -10,7 +10,4 @@ class BookCreate(BookBase):
     pass
 
 class Book(BookBase):
-    id: int
-
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
